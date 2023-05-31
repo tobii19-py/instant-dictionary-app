249502d8943e314d22722b3e29a308c8991d386d
tobi <tobibags19@gmail.com>
2023-05-31 14:02:46 -0500

    Display output in the output div on the frontend

--- webapp/dictionary.py
+++ webapp/dictionary.py
@@ -4,17 +4,27 @@ import justpy as jp
 class Dictionary:
     path = "/dictionary"
 
-    def serve(self):
+    @classmethod
+    def serve(cls):
         wp = jp.QuasarPage(tailwind=True)
         div = jp.Div(a=wp, classes="bg-gray-200 h-screen")
         jp.Div(a=div, text="Instant English Dictionary", classes="text-4xl m-2")
         jp.Div(a=div, text="Get the definition of any English word instantly.",
                classes="text-lg")
 
-        jp.Input(a=div, placeholder="Type in a word here...",
+        input_div = jp.Div(a=div, classes="grid grid-cols-2")
+        jp.Input(a=input_div, placeholder="Type in a word here...",
                  classes="m-2 bg-gray-100 border-2 border-gray-200 rounded w-64 "
                          "focus:outline-none focus:border-purple-500 "
                          "py-2 px-4 ")
-        jp.Div(a=div, classes="m-2 p-2 text-lg border-2 border-gray-300 h-40")
+
+        output_div = jp.Div(a=div, classes="m-2 p-2 text-lg border-2 border-gray-300 h-40")
+        jp.Button(a=input_div, text="Get definition",
+                  classes="border-2 border-gray-300 text-gray-500",
+                  click=cls.get_definition, outputdiv=output_div)
 
         return wp
+
+    @staticmethod
+    def get_definition(widget, msg):
+        widget.outputdiv.text = "Something was displayed!"
