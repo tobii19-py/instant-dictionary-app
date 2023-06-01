37b3cb057fdbae31e73dd5b24202d0605cb12d76
tobi <tobibags19@gmail.com>
2023-06-01 13:03:42 -0500

    Add a navigation panel to Home page

--- webapp/home.py
+++ webapp/home.py
@@ -4,9 +4,22 @@ import justpy as jp
 class Home:
     path = "/"
 
-    def serve(self):
+    @classmethod
+    def serve(cls, req):
         wp = jp.QuasarPage(tailwind=True)
-        div = jp.Div(a=wp, classes="bg-gray-200 h-screen")
+
+        layout = jp.QLayout(a=wp, view="hHh lpR fFf")
+        header = jp.QHeader(a=layout)
+        toolbar = jp.QToolbar(a=header)
+
+        drawer = jp.QDrawer(a=layout, show_if_above=True, v_mode="left",
+                            bordered=True)
+        jp.QBtn(a=toolbar, dense=True, flat=True, round=True, icon="menu",
+                click=cls.move_drawer, drawer=drawer)
+        jp.QToolbarTitle(a=toolbar, text="Instant Dictionary")
+
+        container = jp.QPageContainer(a=layout)
+        div = jp.Div(a=container, classes="bg-gray-200 h-screen p-2")
         jp.Div(a=div, text="This is the Home page!", classes="text-4xl m-2")
         jp.Div(a=div, text="""
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
@@ -18,4 +31,12 @@ class Home:
                 amet venenatis urna cursus eget. Pellentesque habitant morbi tristique 
                 senectus. Ullamcorper a lacus vestibulum sed arcu non odio euismod.
                 """, classes="text-lg")
+
         return wp
+
+    @staticmethod
+    def move_drawer(widget, msg):
+        if widget.drawer.value:
+            widget.drawer.value = False
+        else:
+            widget.drawer.value = True
